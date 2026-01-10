7b22d73aaab672f792c37045063aaac9692bcb98
Denis Arsenev <ensasmm@gmail.com>
2026-01-10 14:42:43 +0300

    feat(hero, benefits): add hero slider and benefits sections 🚀✨
    - 🖼️ implemented main hero slider
    - 💡 added benefits info section below hero
    - 🧱 applied BEM structure and styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,49 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// // Галерея и лайтбоксы от Fancybox
+// import { Fancybox } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// Fancybox.bind('[data-fancybox]', {
+// 	// Your custom options
+// });
+
+// // Мобильная навигация
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+//////////////////////////////////////// SWIPER JS ////////////////////////////////////////
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const heroSlider = new Swiper('.hero__slider', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  speed: 800,
+  loop: true,
+
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className}">${index + 1}</span>`;
+    },
+  },
+
+  // navigation: {
+  //   nextEl: '.slider__btn--next',
+  //   prevEl: '.slider__btn--prev',
+  // },
+
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
-
-// Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
